b6e08dcd84f4e4292c4c6cd7a7c8c89a8b0b97c0
chambab <chambab@gmail.com>
2012-12-02 22:35:47 +0900

    Another Activity Test

---
+++ MyFirstApp/src/com/test/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,31 @@
+package com.test.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_display_message);
+		// Show the Up button in the action bar.
+		
+		Intent intent = getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+		
+		setContentView(textView);
+		
+	}
+
+
+}
