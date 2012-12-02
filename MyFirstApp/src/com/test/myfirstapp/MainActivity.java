1e95f775a2d743c30c1fb4da0bacf6c8c5309db3
chambab <chambab@gmail.com>
2012-12-02 22:33:32 +0900

    Start Another Activity Test

--- MyFirstApp/src/com/test/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/test/myfirstapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.test.myfirstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.test.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +21,20 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    /**
+     * 
+     * @param view
+     */
+    public void sendMessage(View view) {
+    	// TODO
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    	
     }
     
 }
